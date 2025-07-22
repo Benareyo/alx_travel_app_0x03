b030ba674379667222ddee7236a399fc192cf5c0
Betel Yohannes <betelyohannes39@gmail.com>
2025-07-23 01:50:47 +0300

    Your commit message here

---
+++ listings/serializers.py
@@ -0,0 +1,19 @@
+# listings/serializers.py
+
+from rest_framework import serializers
+from .models import Listing, Booking, Review
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = '__all__'
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
